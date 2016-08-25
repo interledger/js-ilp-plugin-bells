dbd359dfb1fd7946face6230546a202048ab859c
sentientwaffle <sentientwaffle@gmail.com>
2016-08-25 13:20:17 -0700

    fix: parse account name correctly for subledgers

--- src/lib/plugin.js
+++ src/lib/plugin.js
@@ -331,7 +331,8 @@ class FiveBellsLedger extends EventEmitter2 {
   }
 
   * _send (transfer) {
-    const sourceAddress = parseAddress(transfer.account)
+    // Strip off the prefix and request id.
+    const sourceUsername = transfer.account.slice(this.prefix.length).split('.')[0]
     const fiveBellsTransfer = omitUndefined({
       id: this.host + '/transfers/' + transfer.id,
       ledger: this.host,
@@ -342,7 +343,7 @@ class FiveBellsLedger extends EventEmitter2 {
         memo: transfer.noteToSelf
       })],
       credits: [omitUndefined({
-        account: this.host + '/accounts/' + encodeURIComponent(sourceAddress.username),
+        account: this.host + '/accounts/' + encodeURIComponent(sourceUsername),
         amount: transfer.amount,
         memo: transfer.data
       })],
@@ -545,14 +546,6 @@ class FiveBellsLedger extends EventEmitter2 {
     for (let i = 0; i < templatePath.length; i++) {
       if (templatePath[i] === ':name') return accountPath[i]
     }
-  }
-}
-
-function parseAddress (address) {
-  const addressParts = address.split('.')
-  return {
-    ledger: addressParts.slice(0, -1).join('.'),
-    username: addressParts[addressParts.length - 1]
   }
 }
 
